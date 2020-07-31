9722c7691de3ea2fd8be83f14b014305fc2651e4
J Arun Mani <j.arunmani@protonmail.com>
2020-08-01 01:20:52 +0530

    Updated start message

--- BookCrushBot/__init__.py
+++ BookCrushBot/__init__.py
@@ -34,21 +34,21 @@ ESCAPE_TABLE = {
 
 DATABASE = psycopg2.connect(os.getenv("DATABASE_URL"))
 
-botm = os.getenv("botm", "True")  # Is BOTM open ?
+botm = os.getenv("BOTM", "True")  # Is BOTM open ?
 if botm == "False":
     BOTM = False
 else:
     BOTM = True
 
-BOTM_LIMIT = int(os.getenv("botm_limit", "2"))  # How many books for BOTM ?
+BOTM_LIMIT = int(os.getenv("BOTM_LIMIT", "2"))  # How many books for BOTM ?
 
-filename = os.getenv("file", None)
+filename = os.getenv("FILE", None)
 if filename:
     FILE = open(filename, "a")
 else:
     FILE = sys.stdout
 
-roulette = os.getenv("roulette", "True")  # Is Roulette open ?
+roulette = os.getenv("ROULETTE", "True")  # Is Roulette open ?
 if roulette == "False":
     ROULETTE = False
 else:
