ff0112e4e1390667af657b9618d45062d0d36cf6
J Arun Mani <j.arunmani@protonmail.com>
2020-08-08 18:53:29 +0530

    reverts

--- BookCrushBot/__main__.py
+++ BookCrushBot/__main__.py
@@ -1,28 +1,4 @@
-import http.server as server
-from threading import Thread
 import BookCrushBot
 
-
-class Handler(server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        print("GET")
-        self.send_response(200, "OK")
-        self.end_headers()
-    def do_POST(self):
-        print("POST")
-        print(self.path)
-        print(self.requestline)
-        self.send_response(200, "OK")
-        self.end_headers()
-
-
-serverd = server.HTTPServer(("", BookCrushBot.PORT), Handler)
-thread = Thread(target=serverd.serve_forever, daemon=True)
-
-print("Starting")
-thread.start()
-
-#loop = BookCrushBot.Loop()
-#loop.run()
-
-thread.join()
+loop = BookCrushBot.Loop()
+loop.run()
