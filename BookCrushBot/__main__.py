62979a352bf53e052bd605ac9e9254313e5ea99e
J Arun Mani <j.arunmani@protonmail.com>
2020-08-08 17:53:47 +0530

    add OK path

--- BookCrushBot/__main__.py
+++ BookCrushBot/__main__.py
@@ -5,12 +5,17 @@ import BookCrushBot
 
 class Handler(server.BaseHTTPRequestHandler):
     def do_GET(self):
+        print("GET")
         if self.path == "/":
             self.send_response(200, "OK")
             self.end_headers()
+    def do_POST(self):
+        print("POST")
+        print(self.path)
+        print(self.requestline)
 
 
-serverd = server.HTTPServer(("", 8000), Handler)
+serverd = server.HTTPServer(("", BookCrushBot.PORT), Handler)
 thread = Thread(target=serverd.serve_forever, daemon=True)
 
 print("Starting")
