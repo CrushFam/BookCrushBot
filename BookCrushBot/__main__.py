2ebbe02b4fc58068b9c8738d34caabc8a209adf3
J Arun Mani <j.arunmani@protonmail.com>
2020-08-08 17:37:31 +0530

    add OK path

--- BookCrushBot/__main__.py
+++ BookCrushBot/__main__.py
@@ -1,4 +1,22 @@
+import http.server as server
+from threading import Thread
 import BookCrushBot
+
+
+class Handler(server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200, "OK")
+            self.end_headers()
+
+
+serverd = server.HTTPServer(("", 8000), Handler)
+thread = Thread(target=serverd.serve_forever, daemon=True)
+
+print("Starting")
+thread.start()
 
 loop = BookCrushBot.Loop()
 loop.run()
+
+thread.join()
