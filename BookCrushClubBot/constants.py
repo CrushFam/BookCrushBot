8f6c0dfb81b2d2488afb5598803a4722bc234741
J Arun Mani <j.arunmani@protonmail.com>
2021-05-31 22:25:49 +0530

    Pride June

--- BookCrushClubBot/constants.py
+++ BookCrushClubBot/constants.py
@@ -5,12 +5,12 @@ class Constants:
 
     FICTION_COUNT = int(getenv("FICTION_COUNT", "2"))
 
-    FICTION_GENRE = "Magical May"
+    FICTION_GENRE = "Pride Month"
 
     FICTION_SESSION = "FICTION"
 
     NONFICTION_COUNT = int(getenv("NONFICTION_COUNT", "2"))
 
-    NONFICTION_GENRE = "Murderous May"
+    NONFICTION_GENRE = "Pride Month"
 
     NONFICTION_SESSION = "NONFICTION"
