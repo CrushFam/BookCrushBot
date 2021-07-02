3523119273784b788ca3c9f25ed57109c7ae48c7
J Arun Mani <j.arunmani@protonmail.com>
2021-07-03 00:43:38 +0530

    Graphic novel

--- BookCrushClubBot/constants.py
+++ BookCrushClubBot/constants.py
@@ -5,12 +5,12 @@ class Constants:
 
     FICTION_COUNT = int(getenv("FICTION_COUNT", "2"))
 
-    FICTION_GENRE = "Pride Month"
+    FICTION_GENRE = "Graphic Novel"
 
     FICTION_SESSION = "FICTION"
 
     NONFICTION_COUNT = int(getenv("NONFICTION_COUNT", "2"))
 
-    NONFICTION_GENRE = "Pride Month"
+    NONFICTION_GENRE = "Graphic Novel"
 
     NONFICTION_SESSION = "NONFICTION"
