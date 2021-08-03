3b30741f445013db7b497bd32b7429b9413f87b8
J Arun Mani <j.arunmani@protonmail.com>
2021-08-03 13:58:53 +0530

    August BOTM

--- BookCrushClubBot/constants.py
+++ BookCrushClubBot/constants.py
@@ -7,13 +7,13 @@ class Constants:
 
     FICTION_COUNT = int(getenv("FICTION_COUNT", "2"))
 
-    FICTION_GENRE = "Graphic Novel"
+    FICTION_GENRE = "Humorous August"
 
     FICTION_SESSION = "FICTION"
 
     NONFICTION_COUNT = int(getenv("NONFICTION_COUNT", "2"))
 
-    NONFICTION_GENRE = "Graphic Novel"
+    NONFICTION_GENRE = "Humorous August"
 
     NONFICTION_SESSION = "NONFICTION"
 
