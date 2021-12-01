155664619f47c0695171f6b6e1d36d0e5f29ea34
Arun Mani J <j.arunmani@protonmail.com>
2021-12-01 21:28:46 +0530

    Updated genre

--- BookCrushClubBot/constants.py
+++ BookCrushClubBot/constants.py
@@ -9,13 +9,13 @@ class Constants:
 
     FICTION_COUNT = int(getenv("FICTION_COUNT", "2"))
 
-    FICTION_GENRE = "Historical Fiction"
+    FICTION_GENRE = "Christmas Fiction"
 
     FICTION_SESSION = "FICTION"
 
     NONFICTION_COUNT = int(getenv("NONFICTION_COUNT", "2"))
 
-    NONFICTION_GENRE = "Historical NonFiction"
+    NONFICTION_GENRE = "Christmas NonFiction"
 
     NONFICTION_SESSION = "NONFICTION"
 
